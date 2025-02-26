8815c980abc0c1427b71af0eb5d2be1bd3fb0bce
ArpitCS <arpit2005.garg@gmail.com>
2025-02-26 14:08:27 +0530

    Fixed Morgan Module Logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -6,14 +6,10 @@ const logger = (req, res, next) => {
   const url = req.url;
   const timestamp = new Date().toISOString();
 
-  // console.log(`[${timestamp}] ${method}:${url}`)
-
   // Sample Code from Morgan Docs
-  const morganFormat = `:method :url :status - :response-time ms`;
-  const morganLogger = morgan(morganFormat);
-
+  const morganLogger = morgan("dev");
   morganLogger(req, res, next);
 
-  // next()
+  // next() // => Called using the Morgan Module Object
 };
 module.exports = logger;
