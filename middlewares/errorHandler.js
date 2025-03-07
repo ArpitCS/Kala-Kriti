dbd8da7f1c54cd5d37045d5ff8c0304eeff86be7
ArpitCS <arpit2005.garg@gmail.com>
2025-03-07 12:55:28 +0530

    Morgan Logger Update

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 // Error Handler
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack)
+    
     res.status(500).send({
         error: 'Something went wrong!',
         message: err.message || 'Internal Server Error'
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,15 +1,34 @@
 // Custom Logger Middleware with Morgan Module
 const morgan = require("morgan");
 
+morgan.token('colored-status', (req, res) => {
+  const status = res.statusCode;
+  let color;
+
+  if (status >= 500) {
+    color = '\x1b[31m';
+  } else if (status >= 400) {
+    color = '\x1b[33m'; 
+  } else if (status >= 300) {
+    color = '\x1b[36m'; 
+  } else if (status >= 200) {
+    color = '\x1b[32m'; 
+  } else {
+    color = '\x1b[0m';  
+  }
+  
+  return `${color}${status}\x1b[0m`;
+});
+
+morgan.token('timestamp', () => {
+  return new Date().toISOString();
+});
+
 const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const timestamp = new Date().toISOString();
+  
+  const morganLogger = morgan("[:timestamp] :method :url :colored-status :response-time ms - :res[content-length]");
 
-  // Sample Code from Morgan Docs
-  const morganLogger = morgan("dev");
   morganLogger(req, res, next);
-
-  // next() // => Called using the Morgan Module Object
 };
+
 module.exports = logger;
