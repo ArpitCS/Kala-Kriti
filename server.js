d03430b92e69570bdc60a526277558c66d1ace92
Sajal Garg <sajalgarg9646@gmail.com>
2025-03-07 09:11:28 +0530

    GET /error Update

--- server.js
+++ server.js
@@ -78,6 +78,10 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "register.html"));
 });
 
+app.get("/error", (req, res) => {
+  errorHandler(new Error("An Error Occurred!"), req, res);
+});
+
 app.get("/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "dashboard.html"));
 });
